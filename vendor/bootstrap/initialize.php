751fe753d96600f22a702326d7f2f82c7821ea18
naofbear <huakaiquan@qq.com>
2017-09-27 16:54:46 +0800

    持续更新

--- vendor/bootstrap/initialize.php
+++ vendor/bootstrap/initialize.php
@@ -14,93 +14,15 @@ $auto = require_once C_VENDOR."autoload.php";
 //收到请求 -- 注入项目
 di()['request']->register();
 
-
-
-
 //监听异常
 services\abnormal::listen();
 
-
-
+//监听中间件
+//\services\middleware::listen();
 
 //路由监听
 //\services\route::listen();
 
-
-//try
-//{
-//    $a = \services\config::get('restriction');
-//    $klein = new \Klein\Klein();
-//
-//    $klein->respond('/[:api]/[:version]/[:action]', function ($request) {
-//        print_r($request);
-//
-//    });
-//
-//    $klein->dispatch();
-//}
-//catch (\Exception $e)
-//{
-//    throw new \coffee\exception\systemError($e->getMessage(),$e->getCode());
-//}
-
-
-
-
-
-//监听请求
-
 //监听防火墙
 
-//监听中间件
-
-//监听路由
-
 //监听响应
-
-
-
-//error_reporting(E_ALL);
-//ini_set('display_errors',1);
-//
-////请求执行 获取项目配置文件
-//\services\config::set('config2');
-
-//print_r(di());
-//di()->setConfig(C_ITEM.'config'.DIRECTORY_SEPARATOR.'config.php');
-//
-//if(!di()->getConfig())
-
-//print_r($config);
-
-//
-//$w = new \Whoops\Handler\JsonResponseHandler();
-//
-//
-//(new \Whoops\Run)->pushHandler($w)->register();
-//
-//
-//di()->setConfig([1,2,3,4,5]);
-//
-//print_r(di()->getConfig());
-//
-//di()->xxx(0);
-
-
-
-//载入配置文件
-//$config = require_once C_ITEM.'config/config.php';
-
-//print_r($config);
-
-
-
-
-
-//    throw new Exception("路由无匹配项 404 Not Found");
-//
-//    print_r(new Illuminate\Database\Capsule\Manager);
-    
-//new coffee\test();
-
-//print_r($_SERVER);
