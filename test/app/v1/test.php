fae81ae2baee6f8c3c828869cd9c8ecff4e348dd
naofbear <huakaiquan@qq.com>
2017-11-07 16:50:53 +0800

    完善

--- test/app/v1/test.php
+++ test/app/v1/test.php
@@ -2,16 +2,10 @@
 
 namespace test\app\v1;
 
-use \services\db;
-
 class test
 {
     public function index()
     {
-//        raise('error',1000);
-        return db::connect()->user('id',[1,2,3])->fetchAll('id');
-
-//        foreach ($a as $v){print_r($v['user']);}
-        print_r($a);
+        raise('welcome to coffee restful api framework.');
     }
 }
