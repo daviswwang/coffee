cf1557c9fa2a946d0bfa0af6be82433cd40b56b2
naofbear <huakaiquan@qq.com>
2017-11-10 17:11:55 +0800

    调试XSS过滤

--- test/app/index.php
+++ test/app/index.php
@@ -2,10 +2,13 @@
 
 namespace test\app;
 
+use services\input;
+
 class index
 {
     public function index()
     {
+        print_r(input::get('a','',true));
         raise('welcome to coffee restful api framework.');
     }
 }
