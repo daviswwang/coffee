14728bb97363dc1e48ee82bc7b2f9b11595ea082
naofbear <huakaiquan@qq.com>
2017-11-15 14:09:39 +0800

    优化修复部分问题

--- test/config/database.php
+++ test/config/database.php
@@ -2,15 +2,13 @@
 
 return[
     'default'=>[
-        'driver'    => 'mysql',
-        'host'      => 'xddnat.xiaodian.in',
-        'database'  => 'wm',
-        'username'  => 'wm',
-        'password'  => 'Wm2017Repai',
-        'port'      => 33061,
+        'driver'    => '',
+        'host'      => '',
+        'database'  => '',
+        'username'  => '',
+        'password'  => '',
+        'port'      => 3306,
         'charset'   => 'utf8',
-        'prefix'    => 'chain_',
-        'primary'   => 'id',
-        'foreign'   => '%s_id'
+        'prefix'    => ''
     ]
 ];
---
+++ vendor/services/cache.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace services;
+
+use coffee\exception\cacheError;
+
+class cache
+{
+
+    private static $cache_pool = [];
+
+    public static function connect($conf = 'default')
+    {
+
+        if(isset(self::$cache_pool[$conf])) return self::$cache_pool[$conf];
+
+        //得到对应数据库配置
+        $set = config::get($conf,'cache');
+
+        if(!$set) throw new cacheError('cache config is error.');
+
+        switch($set['driver'])
+        {
+            case 'memcached':
+                    $cache = new \Memcached();
+                    $cache->addServer($set['host'],$set['port']);
+                    self::$cache_pool[$conf] = $cache;
+                break;
+        }
+
+        if(!isset(self::$cache_pool[$conf])) throw new cacheError('new cache object is error.');
+
+        return $cache;
+    }
+
+    public static function disconnect($conf = 'default')
+    {
+        unset(self::$db_pool[$conf]);
+        return true;
+    }
+}
